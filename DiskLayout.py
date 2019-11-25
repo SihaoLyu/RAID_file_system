c4b7c89a8519b104ac5d2f3e5219018749174e96
SihaoLyu <54587468+SihaoLyu@users.noreply.github.com>
2019-11-25 00:37:24 -0500

    Update DiskLayout.py
    Add attribute of WHOLE_BLK_SIZE in SuperBlock class

--- DiskLayout.py
+++ DiskLayout.py
@@ -8,6 +8,7 @@ class SuperBlock():
     def __init__(self):
         self.TOTAL_NO_OF_BLOCKS = config.TOTAL_NO_OF_BLOCKS                     #DECIDED BY CONFIG FILE
         self.BLOCK_SIZE = config.BLOCK_SIZE                                     #DECIDED BY CONFIG FILE
+        self.WHOLE_BLOCK_SIZE = config.WHOLE_BLOCK_SIZE                         #DECDIED BY CONFIG FILE
         self.MAX_NUM_INODES = config.MAX_NUM_INODES                             #DECIDED BY CONFIG FILE
         self.INODE_SIZE = config.INODE_SIZE                                     #DECIDED BY CONFIG FILE
         self.INODES_PER_BLOCK = config.BLOCK_SIZE / config.INODE_SIZE           #CALCULATED 
